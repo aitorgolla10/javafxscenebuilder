f62081f1b5b4e827fec8709a985bb4520186c1a4
Aitor Gomez Llavori <agomez257@ikasle.ehu.eus>
2019-10-14 10:10:50 +0200

    as

--- src/main/java/ehu/isad/Controller.java
+++ src/main/java/ehu/isad/Controller.java
@@ -3,7 +3,9 @@ package ehu.isad;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
@@ -13,6 +15,9 @@ public class Controller {
 
   // Reference to the main application.
   private Main mainApp;
+
+  @FXML
+  private CheckBox ikasleMarka;
 
   @FXML
   private TextField txtErabiltzaile;
@@ -26,6 +31,15 @@ public class Controller {
 
   @FXML
   public void onClick(ActionEvent actionEvent) {
-    System.out.println(txtErabiltzaile.getText() + ":" + txtPasahitza.getText());
+    if (ikasleMarka.isSelected()){
+      System.out.println("Ikaslea zara");
+    }
+    else{
+      System.out.println("Ez zara ikaslea");
+    }
+  }
+
+  public void irtenSakatu(ActionEvent actionEvent) {
+    System.exit(0);
   }
 }
